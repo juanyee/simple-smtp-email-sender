9f1f00defb958bd46dac6f5d1fa286e24e2258ea
Juan Yee <juan.yee@unosquare.com>
2022-01-26 14:58:11 -0600

    removing comments and adding HTML as a content subtype

--- sender.py
+++ sender.py
@@ -6,9 +6,9 @@ msg = EmailMessage()
 msg['Subject'] = f'Test'
 msg['From'] = var.FROM_EMAIL
 msg['To'] = ', '.join(var.TO_EMAILS)
-msg.set_content(f"Hello")
-with smtplib.SMTP(var.SERVER, var.PORT) as s: # pending to remove "587"
-    s.starttls() # pending to remove
-    s.login(var.SENDER, var.PASSWORD) # pending to remove
+msg.set_content(f"Hello", subtype='html')
+with smtplib.SMTP(var.SERVER, var.PORT) as s:
+    s.starttls()
+    s.login(var.SENDER, var.PASSWORD)
     s.send_message(msg)
     s.quit()
